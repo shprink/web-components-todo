36b9c05a8dce973e7cc049ecf01ea84da4784016
Trey Shugart <treshugart@gmail.com>
2018-02-20 21:55:35 +1100

    Rename to better represent the project + renderer.

---
+++ skatejs-lit-html/src/index.js
@@ -0,0 +1,7 @@
+import App from "./app";
+import Input from "./input";
+import Item from "./item";
+
+customElements.define("todo-app", App);
+customElements.define("todo-input", Input);
+customElements.define("todo-item", Item);
