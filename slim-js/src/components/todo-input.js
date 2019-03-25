a180a12e6fa19f9e14352f8e6dfd64afb01235d8
eavichay <eavichay@gmail.com>
2019-03-25 17:41:33 +0200

    upgrade to slim 4 and rollup

--- slim-js/src/components/todo-input.js
+++ slim-js/src/components/todo-input.js
@@ -3,21 +3,40 @@ import { Slim } from "slim-js";
 
 @tag('todo-input')
 @template(/*html*/`
-  <style>${require('./todo-input.css')}</style>
-  <form submit="onSubmit">
-    <input s:id="input" type="text" placeholder="What needs to be done?"/>
-  </form>
+<style>
+:host, :host * {
+  font-family: inherit;
+  font-weight: inherit;
+  font-size: 24px;
+}
+form {
+  border-bottom: 1px solid #ededed;
+}
+
+input {
+  position: relative;
+  margin: 0 0 2px 0;
+  width: 100%;
+  line-height: 1.4em;
+  outline: none;
+  padding: 6px;
+  border: 1px solid #CCC;
+  box-shadow: 0 0px 1px 0px rgba(0, 0, 0, 0.5);
+  box-sizing: border-box;
+}
+</style>
+<form submit="onSubmit">
+  <input s:id="input" type="text" placeholder="What needs to be done?"/>
+</form>
 `)
 @useShadow(true)
 export default class extends Slim {
-  onSubmit (e) {
-    const { input } = this
-    input.value = input.value.trim()
-    const { value: text } = input
+  onSubmit(e) {
     e.preventDefault()
+    const text = this.input.value.trim()
     if (text) {
       this.callAttribute('on-new-item', text)
-      input.value = null
+      this.input.value = null
     }
   }
 }
